8b9887c96f8056d0ec47d7aacf8cdeb7ce035eb2
d.pavlenko <d.pavlenko@viseven.com>
2019-04-28 12:53:50 +0300

    add files for heroku

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-/* const formData = require("express-form-data"); */
-/* const os = require("os"); */
 var bodyParser = require('body-parser')
 
 const port = 8009;
@@ -10,17 +8,11 @@ const app = express();
 app.use(express.static('public'));
 
 
-/* const options = {
-    uploadDir: os.tmpdir(),
-    autoClean: true
-}; */
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-/* app.use(formData.parse(options));
-app.use(formData.format());
-app.use(formData.stream());
-app.use(formData.union()); */
+
 
 require('./router')(app);
 
